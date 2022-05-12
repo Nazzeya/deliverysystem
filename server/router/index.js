e06059edad67106ebbbc735e6fd9fb5151343351
Nazzeya <n.baisakalova01@gmail.com>
2022-05-12 18:17:19 +0600

    add uploeder method
    u can upload multiple files

--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,14 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
+const uploadController = require('../controllers/upload');
 const router = new Router();
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 const authMiddleware = require('../middlewares/auth-middleware');
 const roleMiddleware = require('../middlewares/role-middlware');
 
 router.post('/registration',
     body('email').isEmail(),
-    body('password').isLength({min: 3, max: 32}),
+    body('password').isLength({ min: 3, max: 32 }),
     userController.registration
 );
 router.post('/login', userController.login);
@@ -16,6 +17,9 @@ router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
 router.get('/users', authMiddleware, userController.getUsers);
 router.post('/postProducts', roleMiddleware(['ADMIN']), userController.postProducts); //roleMiddleware(["ADMIN"]),
-router.get('/products',userController.getProducts);
+router.get('/products', userController.getProducts);
+
+router.get('/getListFiles', uploadController.getListFiles);
+router.get('/download', uploadController.download);
 
 module.exports = router
